ae0dc145be0b8252aa006c84b7d5e622d033a4e8
Jaak Pruulmann-Vengerfeldt <jjpp@cyber.ee>
2019-12-06 22:28:55 +0100

    R dets   6 22:28:55 CET 2019

---
+++ precache-manifest.0abd7052b51663bf186a7be4ee794e59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04fba8e4d72b82b307c69ef2e37328f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d789d8971713ad49ebf",
+    "url": "./static/css/main.8bd328d8.chunk.css"
+  },
+  {
+    "revision": "df2fde789237aff8a81e",
+    "url": "./static/js/2.b03b7613.chunk.js"
+  },
+  {
+    "revision": "9d789d8971713ad49ebf",
+    "url": "./static/js/main.f8a92c4f.chunk.js"
+  },
+  {
+    "revision": "db7e7bc0f18c265eec55",
+    "url": "./static/js/runtime-main.89186ddc.js"
+  },
+  {
+    "revision": "9f370677288c010dc74625bc360d8dc8",
+    "url": "./static/media/login.9f370677.svg"
+  },
+  {
+    "revision": "bca08fffe2a29b8e33ff27c28de86f20",
+    "url": "./static/media/logo-est.bca08fff.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5947b56d4c45cf5ea0e6dea6a91d01e2",
+    "url": "./static/media/tara-logo-et.5947b56d.png"
+  },
+  {
+    "revision": "bdeaa2619b5981a804c504aa35a812d9",
+    "url": "./static/media/user-icon.bdeaa261.svg"
+  }
+]);
